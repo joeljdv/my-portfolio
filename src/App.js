9b414e7af2e4b9801c4615e61ea12136d1f0c09a
Joel Diaz <joeldiazjdv@gmail.com>
2024-08-08 11:54:27 -0400

    Deploy

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       <div className="pages">
         <BrowserRouter>
           <Routes>
-            <Route path="/" exact element={<Home />} />
+            <Route path="/portfolio" exact element={<Home />} />
             <Route path="/about" exact element={<About />} />
             <Route path="work" exact element={<Work />} />
           </Routes>
